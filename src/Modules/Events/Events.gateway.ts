d22769836453be2128fa8d4f3dea20f69bf2a78f
extendslcc <lucas.lcc@hotmail.com>
2021-10-23 17:36:47 -0300

    feat: setup websocket service with socket.io

---
+++ src/Modules/Events/Events.gateway.ts
@@ -0,0 +1,34 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class EventsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+  private readonly logger = new Logger(EventsGateway.name);
+
+  afterInit(server: Server) {
+    this.logger.log(`${EventsGateway.name} socket started`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { PrismaModule } from 'nestjs-prisma';
 import { AuthModule } from './Modules/AuthUser/Auth.module';
 import { MessagesModule } from './Modules/Messages/Messages.module';
 import { UserModule } from './Modules/Users/User.module';
+import { EventsGateway } from './Modules/Events/Events.gateway';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { UserModule } from './Modules/Users/User.module';
     AuthModule,
     UserModule,
     MessagesModule,
+    EventsGateway,
   ],
   controllers: [],
   providers: [],
