15b9de761a0c14705e1bf8eb6cdbbec610930c16
extendslcc <lucas.lcc@hotmail.com>
2021-10-24 18:52:43 -0300

    feat: implements ui of signin button component

---
+++ react-native/src/components/SignInBox/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './styles';
+import { Button } from '../Button';
+import { COLORS } from '../../theme';
+
+export function SignInBox() {
+  return (
+    <View style={styles.container}>
+      <Button
+        title="ENTRAR COM O GITHUB"
+        color={COLORS.BLACK_PRIMARY}
+        backgroundColor={COLORS.YELLOW}
+        icon="github"
+      />
+    </View>
+  );
+}
--- react-native/src/screens/Home/index.tsx
+++ react-native/src/screens/Home/index.tsx
@@ -4,12 +4,14 @@ import { View } from 'react-native';
 import { styles } from './styles';
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
+import { SignInBox } from '../../components/SignInBox';
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Header />
       <MessageList />
+      <SignInBox />
     </View>
   );
 }
