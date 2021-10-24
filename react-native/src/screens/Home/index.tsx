e54b27e8aff28e185d1921131874d9df8b7c8ce6
extendslcc <lucas.lcc@hotmail.com>
2021-10-24 18:03:29 -0300

    feat: implements header component

--- react-native/src/screens/Home/index.tsx
+++ react-native/src/screens/Home/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 
 import { styles } from './styles';
+import { Header } from '../../components/Header';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>Olá react native</Text>
+      <Header />
     </View>
   );
 }
