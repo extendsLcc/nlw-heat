ebe5168ef23a72a96f6b21e36f57040df8f7bce2
extendslcc <lucas.lcc@hotmail.com>
2021-10-24 02:03:24 -0300

    feat: implements message listing websocket

--- react-web/src/components/MessageList/index.tsx
+++ react-web/src/components/MessageList/index.tsx
@@ -3,6 +3,7 @@ import { api } from '../../servies/api';
 
 import styles from './styles.module.scss';
 import logoImg from '../../assets/logo.svg';
+import { io } from 'socket.io-client';
 
 type Message = {
   id: string;
@@ -13,8 +14,28 @@ type Message = {
   };
 };
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:3000');
+
+socket.on('new_message', (newMessage: Message) => {
+  console.log(newMessage);
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages((prevState) =>
+          [messagesQueue[0], messages[0], messages[1]].filter(Boolean),
+        );
+        messages.shift();
+      }
+    }, 3000);
+  }, []);
 
   useEffect(() => {
     api.get<Message[]>('/messages/last3').then((response) => {
