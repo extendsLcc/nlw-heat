9336798f87e6faae9d6ac3aa312c4cc035b639c7
extendslcc <lucas.lcc@hotmail.com>
2021-10-24 18:29:51 -0300

    feat: add animation to message component

--- react-native/src/components/Message/index.tsx
+++ react-native/src/components/Message/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { MotiView } from 'moti';
 
 import { styles } from './styles';
 import { UserPhoto } from '../UserPhoto';
@@ -19,13 +20,18 @@ type Props = {
 
 export function Message({ data }: Props) {
   return (
-    <View style={styles.container}>
+    <MotiView
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: 'timing', duration: 700 }}
+      style={styles.container}
+    >
       <Text style={styles.message}>{data.text}</Text>
 
       <View style={styles.footer}>
         <UserPhoto imageUri={data.user.avatarUrl} sizes="SMALL" />
         <Text style={styles.userName}>{data.user.name}</Text>
       </View>
-    </View>
+    </MotiView>
   );
 }
