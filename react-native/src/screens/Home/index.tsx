0883be7a2df9d6619d04e787ea5e889c9c902409
extendslcc <lucas.lcc@hotmail.com>
2021-10-24 22:18:12 -0300

    feat: implements realtime messages read with socket.io

--- react-native/src/screens/Home/index.tsx
+++ react-native/src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { KeyboardAvoidingView, Platform, View } from 'react-native';
 
 import { styles } from './styles';
 import { Header } from '../../components/Header';
@@ -11,10 +11,15 @@ import { useAuth } from '../../hooks/auth';
 export function Home() {
   const { user } = useAuth();
   return (
-    <View style={styles.container}>
-      <Header />
-      <MessageList />
-      {user ? <SendMessageForm /> : <SignInBox />}
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <View style={styles.container}>
+        <Header />
+        <MessageList />
+        {user ? <SendMessageForm /> : <SignInBox />}
+      </View>
+    </KeyboardAvoidingView>
   );
 }
